66def579283ca2c88554b299c9c92d7028d8efcc
allahverdia <165601933+allahverdia@users.noreply.github.com>
2026-04-13 15:06:13 -0400

    Create environment_test.py

---
+++ environment_test.py
@@ -0,0 +1,27 @@
+# Not all of the imports below are used, but we find it helpful to include all of them
+
+# Basic constructs:
+from ProductRegisters.FeedbackRegister import FeedbackRegister
+from ProductRegisters.FeedbackFunctions import *
+
+# Boolean logic and chaining templates
+from ProductRegisters.BooleanLogic import *
+from ProductRegisters.BooleanLogic.ChainingGeneration.Templates import *
+import ProductRegisters.BooleanLogic.ChainingGeneration.TemplateBuilding
+
+# Berlekamp-Massey and variants
+from ProductRegisters.Tools.RegisterSynthesis.lfsrSynthesis import *
+from ProductRegisters.Tools.RegisterSynthesis.fcsrSynthesis import *
+from ProductRegisters.Tools.RegisterSynthesis.nlfsrSynthesis import *
+
+# Tools and other extraneous files
+import ProductRegisters.Tools.ResolventSolving as ResolventSolving
+from ProductRegisters.Tools.RootCounting.MonomialProfile import *
+
+# Cryptanalysis:
+from ProductRegisters.Cryptanalysis.Attacks.cube_attacks import *
+from ProductRegisters.Cryptanalysis.utility import *
+
+# This should print if all imports succeed:
+
+print("Environment test successful.")
